2097ead90058cdcd6c3465fabe97febbb02372e4
isabela <silva.isabela29@escola.pr.gov.br>
2025-10-21 09:23:55 -0300

    ?

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+function mostraPalavrasChave(){
+   alert("Fui clicado!") ;
+}
