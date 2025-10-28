f77f875686bdad278c8183849c8869d0902bec80
isabela <silva.isabela29@escola.pr.gov.br>
2025-10-28 10:14:42 -0300

    28/10

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
-function mostraPalavrasChave(){
-   const texto = document.querySelector("#entrada-de-texto").value;
-   const campoResultado = document.querySelector("#resultado-palavrachave");
-   const palavrasChave = processaTexto(texto);
+function mostraPalavrasChave() {
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavrasChave = processaTexto(texto);
 
 
-campoResultado.textContent = palavrasChave.join(", ")
+    campoResultado.textContent = palavrasChave.join(", ")
 }
 
-function processaTexto(texto){
+function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
+    let frequencias = {};
+
+    for (let i of palavras){
+        frequencias[i] = 0;
+
+        for (let j of palavras){
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
     return palavras;
 
 }
