259e8f19d2b26c9eb5d0fb8389d80abf09876b73
isabela <silva.isabela29@escola.pr.gov.br>
2025-11-04 10:20:53 -0300

    11/04

--- script.js
+++ script.js
@@ -44,6 +44,16 @@ function contaFrequencias(palavras) {
     return palavras;
 
 }
-function tiraPalavrasRuins(palavras){
-    const PALAVRAS_RUINS =new Set(["para", "nós", "das", "como", "que"]);
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set(["para", "nós", "das", "como", "que"]);
+    const palavrasBoas = [];
+
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+
+    }
+
+return palavrasBoas;
 }
