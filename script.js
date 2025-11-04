578563fc376f632dd6134d1a87bdc35fe6c417ee
isabela <silva.isabela29@escola.pr.gov.br>
2025-11-04 09:19:35 -0300

    11/04

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function mostraPalavrasChave() {
     campoResultado.textContent = palavrasChave.join(", ")
 }
 
-function processaTexto(texto){
+function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
     const frequencias = contaFrequencias(palavras);
@@ -20,9 +20,11 @@ function ordenaPalavra(p1, p2){
     return frequencias [p2] - frequencias[p1];
 }
 return ordenadas.slice(0,10);
+{
 
+    
 
-function contaFrequencias(palavras){
+function contaFrequencias(palavras) {
   let frequencias = {};
 
     for (let i of palavras){
